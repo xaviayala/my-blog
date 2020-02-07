48b7915da7ea5440e53b38fc90949beb7e968ae3
xayala <47429802+xayala@users.noreply.github.com>
2020-02-07 10:36:37 +0100

    my-blog release

---
+++ src/build/precache-manifest.4c52a86d2ec3fbf84a26e3cb785c07ae.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "13bbf6e398ff0b2c039a6066fbac91af",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a5b74e660cb5a0764f0c",
+    "url": "/static/css/main.4a9d094a.chunk.css"
+  },
+  {
+    "revision": "b4cf3f4a2193fd187a3c",
+    "url": "/static/js/2.7a794197.chunk.js"
+  },
+  {
+    "revision": "1bbc351e47e0505633e3308dcab5f46f",
+    "url": "/static/js/2.7a794197.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a5b74e660cb5a0764f0c",
+    "url": "/static/js/main.40815b22.chunk.js"
+  },
+  {
+    "revision": "1a6842cff40cdbd7ee88",
+    "url": "/static/js/runtime-main.d7f357ff.js"
+  }
+]);
